fa3419691693ae92bf0c565ef1709d04bf355070
tariqul islam <tariqul.islam.rony@gmail.com>
2018-12-16 15:28:58 +0600

    Add the Initlized Project

---
+++ src/test/java/com/example/graphqlstarter/GraphqlStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.graphqlstarter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GraphqlStarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
